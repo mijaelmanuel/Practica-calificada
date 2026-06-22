de079a5abea0dcc84dd735f43f2356401d3d2176
mijaelmanuel <mijaelmg03@gmail.com>
2026-06-22 09:29:07 -0500

    Add files via upload

---
+++ Ejercicio22.java
@@ -0,0 +1,13 @@
+public class Ejercicio22 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 2 != 0) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
