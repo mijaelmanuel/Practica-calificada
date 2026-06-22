46bfe7ff64506d8f24c94574e90c90460d89b51c
mijaelmanuel <mijaelmg03@gmail.com>
2026-06-22 09:27:51 -0500

    Add files via upload

---
+++ Ejercicio21.java
@@ -0,0 +1,13 @@
+public class Ejercicio21 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 50; i++) {
+
+            if (i == 30) {
+                break;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
