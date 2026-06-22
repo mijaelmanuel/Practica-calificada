19313426ae628cb7e5c403261d9cc667b7f18247
mijaelmanuel <mijaelmg03@gmail.com>
2026-06-22 08:37:30 -0500

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,12 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int suma = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+            suma += i;
+        }
+
+        System.out.println("Suma total: " + suma);
+    }
+}
